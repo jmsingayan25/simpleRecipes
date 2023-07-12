1483a61bfd3a3055575e18f7cad12687ed929b4c
John Michael Singayan <jsingayan54@gmail.com>
2023-07-12 19:30:19 +0800

    create tables

--- database/migrations/2023_07_12_093037_create_steps_table.php
+++ database/migrations/2023_07_12_093037_create_steps_table.php
@@ -11,15 +11,11 @@ return new class extends Migration
      */
     public function up(): void
     {
-        Schema::create('users', function (Blueprint $table) {
+        Schema::create('steps', function (Blueprint $table) {
             $table->id();
-            $table->string('first_name');
-            $table->string('last_name');
-            $table->date('birthdate');
-            $table->string('email')->unique();
-            $table->timestamp('email_verified_at')->nullable();
-            $table->string('password');
-            $table->rememberToken();
+            $table->integer('step');
+            $table->string('description');
+            $table->integer('time');
             $table->timestamps();
         });
     }
@@ -29,6 +25,6 @@ return new class extends Migration
      */
     public function down(): void
     {
-        Schema::dropIfExists('users');
+        Schema::dropIfExists('steps');
     }
 };
